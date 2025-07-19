c1f81577c096f1e7a91fecd3c9bb623fb8be26b1
Blake <v.blake.dev@gmail.com>
2025-07-19 22:09:33 +0200

    strifeproof_multiplicator.py
    Beginner Challenge

---
+++ strifeproof_multiplicator.py
@@ -0,0 +1,19 @@
+# Strifeproof Multiplication
+
+# Challenge ( Beginner)
+
+# You are given two numbers as input, num1 and num2.
+
+# Your task is to calculate their product and store the result in a variable named product.
+
+# Check the test cases to see the inputs and the expected outputs.
+
+#######################################################################################################
+
+num1 = int(input()) # Don't change this line
+num2 = int(input()) # Don't change this line
+product = 0
+
+product = num1 * num2
+
+print("product =", product) # Don't change this line
